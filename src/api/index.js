d5f1377510e242f6312cce052e017698516e2d33
lkpan <904465114@qq.com>
2023-01-09 17:40:38 +0800

    完成了轮播图

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,10 @@
 //进行API统一管理
 import requests from "./request";
+import mockRequests from './mockAjax'
 //三级联动接口
 // get
 
 //发请求:axios发请求返回结果是Promise对象
-export const reqCategoryList = ()=>requests({url:'/product/getBaseCategoryList',method:'get'})
+export const reqCategoryList = ()=>requests.get('/product/getBaseCategoryList')
+export const reqGetBannerList = ()=>mockRequests.get('/banner')
 
